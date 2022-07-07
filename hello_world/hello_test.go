0043dbd43e416f20d8b445e955021deb941953cc
fehersanyi-bitrise <sandor.feher@bitrise.io>
2022-07-07 11:44:54 +0200

    added a default valua to name and refactored the tests for better organisation and readability

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello("Sanyi")
-	want := "Hello, Sanyi"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	handleAssertion := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("Sanyi")
+		want := "Hello, Sanyi"
+
+		handleAssertion(t, got, want)
+	})
+	t.Run("default to world if empty", func(t *testing.T) {
+		got := hello("")
+		want := "Hello, world"
+
+		handleAssertion(t, got, want)
+	})
 }
