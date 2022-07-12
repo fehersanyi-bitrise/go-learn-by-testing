d109440d5a514403855648fad789ea2c2be1fd01
fehersanyi-bitrise <sandor.feher@bitrise.io>
2022-07-12 11:10:57 +0200

    finished hello world excerscise

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+const hungarian = "Hungarian"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const hungarianHelloPrefix = "Szevasz, "
+const frenchHelloPrefix = "Bonjour, "
 
-func hello(name string) string {
+func hello(name string, lang string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	return languagePrefix((lang)) + name
+}
+
+func languagePrefix(lang string) (prefix string) {
+	switch lang {
+	case hungarian:
+		prefix = hungarianHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(hello("world"))
+	fmt.Println(hello("world", "English"))
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -12,14 +12,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := hello("Sanyi")
+		got := hello("Sanyi", "English")
 		want := "Hello, Sanyi"
 
 		handleAssertion(t, got, want)
 	})
 	t.Run("default to world if empty", func(t *testing.T) {
-		got := hello("")
+		got := hello("", "English")
 		want := "Hello, world"
+
+		handleAssertion(t, got, want)
+	})
+	t.Run("hungarian support", func(t *testing.T) {
+		got := hello("Bélám", "Hungarian")
+		want := "Szevasz, Bélám"
+
+		handleAssertion(t, got, want)
+	})
+	t.Run("french support", func(t *testing.T) {
+		got := hello("Belle", "French")
+		want := "Bonjour, Belle"
 
 		handleAssertion(t, got, want)
 	})
